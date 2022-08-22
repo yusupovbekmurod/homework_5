8063ad8a74f24e752d0a78c579f10f5947e31a01
yusupovbekmurod <bekmurodyusupov81@gmail.com>
2022-08-22 21:29:21 +0500

    java_home

---
+++ main.js
@@ -0,0 +1,190 @@
+"use strict";
+
+// =============== 1-misol ===============
+
+// let n= 10;
+// let array = [];
+
+// for (let i=1; i<=n; i++){
+//   console.log( array[i]=i)
+// }
+
+// =============== 2-misol ===============
+// let n= 10;
+// let array = [];
+
+// for (let i=1; i<=n; i++){
+//   console.log( array[i]=Math.pow(2,i))
+// }
+
+// =============== 3-misol ===============
+
+// let n =+prompt("n=");
+// let sum = 0;
+// let array = [];
+
+// for (let i = 0; i <= n; i++) {
+//    sum = sum + i;
+
+// }
+// console.log(array[sum]=sum)
+
+// =============== 4-misol ===============
+
+// let array=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let sum=0;
+// let num=0;
+// for(let i=0; i<array.length; i++){
+
+//    if(i % 2 == 0){
+//     sum=sum-array[i]
+//       console.log(sum)
+
+//    }else{
+//         num=num+array[i]
+//        console.log(num)
+//    }
+
+// }
+
+// =============== 5-misol ===============
+
+// let n = +prompt("n=");
+// let array = [];
+// let sum = 0;
+// let num = 0;
+
+// for (let i = 0; i < n; i++) {
+//   array[i] = i;
+//   num = +prompt("num=");
+//   array[i] =sum+=num;
+// }
+// console.log(array)
+
+// ====================N2==============
+// let n=6;
+
+// let arr=[];
+// let sum=0;
+
+// for(let i=0; i<=n; i++){
+//    let pr = +prompt(`Son kiriting ${i}`)
+//    arr[i]=pr
+// }
+// for(let j=0; j< arr.length; j++){
+//    sum+=arr[j]
+// }
+// console.log(arr)
+// console.log(sum)
+
+
+// =============== 6-misol ===============
+
+// let n =+prompt("n=");
+// let array=[];
+// for (let i = n; i >= 1; i--) {
+//   console.log(array[i]=i)
+
+// }
+
+// =============== 7-misol ===============
+
+// let array = ["Abduhamid", 17, "Muhammadamin", 16,"Jovohir", 19, "Abdulla",24, "Boboqul", 48];
+
+// console.log("Abduhamid "+array[1]+" yosh "+" Abdulla "+array[7]+" yosh "+" farqi "+" Abduhamid " +(24-17)+ " yosh kichik Abdulladan ")
+
+// =============== 8-misol ===============
+// let sum=0;
+//let array =[];
+// let K = +prompt("K=");
+// let L = +prompt("L=");
+// for (let j = K; j < L; j++) {
+// sum+=array[j];
+// }
+// console.log(sum);
+
+// =============== 9-misol ===============
+// let n = +prompt("n=");
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 0) {
+//    sum = i
+//   } else {
+//     console.log();
+//   }
+// }
+// console.log(sum)
+
+// =============== 10-misol ===============
+// let n = +prompt("n=");
+// let sum = 2;
+
+// for (let i = 0; i < n; i++) {
+//   if (i % 2 == 0) {
+//    sum==Math.min(i)
+//   } else {
+//     console.log();
+//   }
+// }
+// console.log(sum)
+
+// =============== 11-misol ===============
+// let n = +prompt("n=");
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 0) {
+//    console.log();
+//   } else {
+//      sum = (Math.max(i))
+//   }
+// }
+// console.log(sum)
+
+// =============== 12-misol ===============
+
+// let sum=0;
+// let max=0;
+// let n=+prompt("nechi marta son kiritmoqchisan: ");
+// for(let i=1; i<=n; i++){
+//    let num=+prompt("Sonlarni kiriting: ");
+
+//    sum+=num;
+//    max = i
+// }
+// console.log((sum/max)+" o'rta arifmetigi")
+
+
+// =============== 13-misol ===============
+
+// let n = +prompt("n=");
+// let sum = 0;
+// let array=[];
+
+// for (let i = 0; i < n; i++) {
+//   if (i % 2 == 0) {
+//     console.log();
+//   } else {
+//     console.log(array[i]=i);
+//   }
+// }
+
+// =============== 14-misol ===============
+// =============== 15-misol ===============
+
+// let array=[0, 2, 5, 3, 9, 7, 13, 12, 99, 54, 33];
+
+// for(let i=0; i<=array.length; i++){
+//   if(array[i]%2==1){
+//     console.log(array[i])
+//   }else{
+//     console.log()
+//   }
+// }
+
+
+
+
+
+
